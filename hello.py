117707c311cc4a38505e87fd6e1d175951263e51
AishaKhalfan <aishakhalfan45@gmail.com>
2023-06-25 22:28:38 +0300

    Templates, Bootstsrap Navbars and Links

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key no one is supposed to know"
+
+#if __name__ == "__main__":
+    #app.run(debug=True)
+# Create A FORM CLASS
+class NameForm(FlaskForm):
+    name = StringField("Whats your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 #create a route decorator
 @app.route('/')
@@ -40,3 +51,16 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",
+                           name = name,
+                           form = form)
