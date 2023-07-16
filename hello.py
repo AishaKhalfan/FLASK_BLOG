75333db8e558a3407ee0531b234b1945cf13223b
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-17 00:58:58 +0300

    Flash Messages and alerts

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -14,20 +14,38 @@ class NameForm(FlaskForm):
     name = StringField("Whats your name", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+# BooleanField
+# DateField
+# DateTimeField
+# DecimalField
+# FileField
+# HiddenField
+# MultipleField
+# FieldField
+# FloatField
+# FormField
+# IntegerField
+# PasswordField
+# RadioField
+# SelectField
+
 #create a route decorator
 @app.route('/')
 #def index():
     #return "<h1>Hello World!</h1>"
-#FILTERS
+
+# FILTERS
 #safe
 #capitalize
 #lower
 #upper
-#title
-#trim
-#striptags
+#title-capitalize each 1st letter in every word
+#trim- remove trailing spaces at the end
+#striptags-remove html strip tags
+
 def index():
     first_name = "Aisha"
+    flash("WELCOME TO OUR WEBSITE")
     stuff = "This is bold text"
     favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
     return render_template("index.html", 
@@ -61,6 +79,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form Submitted Successfully")
     return render_template("name.html",
                            name = name,
                            form = form)
